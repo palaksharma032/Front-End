4b70d6a0c4e1b207e9503879083268e29389ef14
Palak Sharma <palaksharma03210@gmail.com>
2026-07-11 23:09:56 +0530

    Add files via upload

---
+++ Quizgame/script.js
@@ -0,0 +1,181 @@
+// DOM Elements
+const startScreen = document.getElementById("start-screen");
+const quizScreen = document.getElementById("quiz-screen");
+const resultScreen = document.getElementById("result-screen");
+const startButton = document.getElementById("start-btn");
+const questionText = document.getElementById("question-text");
+const answersContainer = document.getElementById("answer-container");
+const currentQuestionSpan = document.getElementById("current-question");
+const scoreSpan = document.getElementById("score");
+const finalScoreSpan = document.getElementById("final-score");
+const maxScoreSpan = document.getElementById("max-score");
+const resultMessage = document.getElementById("result-message");
+const restartButton = document.getElementById("restart-btn");
+const progressBar = document.getElementById("progress");
+
+const quizQuestions = [
+  {
+    question: "What is the only mammal capable of true flight?",
+    answers: [
+      { text: "Flying Squirel", correct: false },
+      { text: "Bat", correct: true },
+      { text: "Eagle", correct: false },
+      { text: "Penguin", correct: false },
+    ],
+  },
+  {
+    question: "How many hearts does an octopus have?",
+    answers: [
+      { text: "1", correct: false },
+      { text: "2", correct: false },
+      { text: "3", correct: true },
+      { text: "4", correct: false },
+    ],
+  },
+  {
+    question: "Which country is famous for inventing Pizza?",
+    answers: [
+      { text: "France", correct: false },
+      { text: "Greece", correct: false },
+      { text: "Italy", correct: true },
+      { text: "Spain", correct: false },
+    ],
+  },
+  {
+    question: "Which is the rarest blood type in the world?",
+    answers: [
+      { text: "O+", correct: false },
+      { text: "AB-", correct: true },
+      { text: "B-", correct: false },
+      { text: "A-", correct: false },
+    ],
+  },
+  {
+    question: "What is largest desert on Earth?",
+    answers: [
+      { text: "Sahara", correct: false },
+      { text: "Gobi", correct: false },
+      { text: "Antarctic", correct: true },
+      { text: "Arabian", correct: false },
+    ],
+  },
+];
+
+// QUIZ STATE VARS
+let currentQuestionIndex = 0;
+let score = 0;
+let answersDisabled = false;
+
+maxScoreSpan.textContent = quizQuestions.length;
+
+// event listeners
+
+startButton.addEventListener("click", startQuiz);
+restartButton.addEventListener("click", restartQuiz);
+
+function startQuiz() {
+
+  // reset variables
+
+  currentQuestionIndex = 0;
+  score = 0;
+  scoreSpan.textContent = 0;
+
+  startScreen.classList.remove("active");
+  quizScreen.classList.add("active");
+
+  showQuestion();
+}
+
+function showQuestion() {
+
+  // reset state
+
+  answersDisabled = false;
+
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+
+  currentQuestionSpan.textContent = currentQuestionIndex + 1;
+
+  const progressPercent = (currentQuestionIndex / quizQuestions.length) * 100;
+  progressBar.style.width = progressPercent + "%";
+
+  questionText.textContent = currentQuestion.question;
+
+  answersContainer.innerHTML = "";
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.textContent = answer.text;
+    button.classList.add("answer-btn");
+
+    button.dataset.correct = answer.correct;
+
+    button.addEventListener("click", selectAnswer);
+
+    answersContainer.appendChild(button);
+  });
+}
+
+function selectAnswer(event) {
+
+  // optimization
+
+  if (answersDisabled) return;
+
+  answersDisabled = true;
+
+  const selectedButton = event.target;
+  const isCorrect = selectedButton.dataset.correct === "true";
+
+  Array.from(answersContainer.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    } else if (button === selectedButton) {
+      button.classList.add("incorrect");
+    }
+  });
+
+  if (isCorrect) {
+    score++;
+    scoreSpan.textContent = score;
+  }
+
+  setTimeout(() => {
+    currentQuestionIndex++;
+
+    // check if there are more questions 
+    if (currentQuestionIndex < quizQuestions.length) {
+      showQuestion();
+    } else {
+      showResults();
+    }
+  }, 1000);
+}
+
+function showResults() {
+  quizScreen.classList.remove("active");
+  resultScreen.classList.add("active");
+
+  finalScoreSpan.textContent = score;
+
+  const percentage = (score / quizQuestions.length) * 100;
+
+  if (percentage === 100) {
+    resultMessage.textContent = "Wow! You're a genius!";
+  } else if (percentage >= 80) {
+    resultMessage.textContent = "Great job! You did really well!";
+  } else if (percentage >= 60) {
+    resultMessage.textContent = "Good effort! Keep learning!";
+  } else if (percentage >= 40) {
+    resultMessage.textContent = "Not bad! Keep Practicing!";
+  } else {
+    resultMessage.textContent = "Keep learning! You'll get better!";
+  }
+}
+
+function restartQuiz() {
+  resultScreen.classList.remove("active");
+
+  startQuiz();
+}
